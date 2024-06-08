1c6355cfa18c142b570d379ac8efb75f6ebc3f8c
Vo Tuong Long <volong2004@gmail.com>
2024-06-08 16:10:38 +0700

    test

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const DB_KEY = process.env.db_key;
 const mongoose = require('mongoose');
-const url = `mongodb+srv://volong2004:${DB_KEY}@mymongodb.whgnern.mongodb.net/blog_dev?retryWrites=true&w=majority&appName=mymongodb`;
+const url = `mongodb+srv://volong2004:khung658@mymongodb.whgnern.mongodb.net/blog_dev?retryWrites=true&w=majority&appName=mymongodb`;
 mongoose.set('strictQuery', false);
 
 
